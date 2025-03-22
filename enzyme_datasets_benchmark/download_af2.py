f24f1a5a4afcb2d3156b4d4742488e602736364a
Ariane Mora <ariane.n.mora@gmail.com>
2025-03-22 12:32:33 -0700

    Fixed threading

--- enzyme_datasets_benchmark/download_af2.py
+++ enzyme_datasets_benchmark/download_af2.py
@@ -4,9 +4,11 @@ import pandas as pd
 from multiprocessing.dummy import Pool as ThreadPool
 import requests
 from docko import *
-        
+import numpy as np
+   
 # Now lets run the download in paralell 
 def run(uniprot_accessions):
+    print("Downloading ", len(uniprot_accessions))
     for uniprot_accession in uniprot_accessions:
         output_file = f'{output_dir}{uniprot_accession}.pdb'
         get_alphafold_structure(uniprot_accession, output_file)
@@ -16,19 +18,22 @@ runs = []
 s_df = pd.read_csv('Low30_mmseq_ID_exp_subset_train.csv')#, sep='\t', header=None)
 output_dir = '/disk1/ariane/vscode/squidly/enzyme_datasets_benchmark/structures/'
 to_download = []
+
+n_threads = 40
 files = list(os.listdir(output_dir))
-entries = set([f.split('.')[0] for f in files])
-for e in s_df['Entry'].values:
-    print(e)
-    if e not in entries:
-        print(e)
-        to_download.append(e)
+df_split = np.array_split(s_df, n_threads)
+
+for df in df_split:
+    chunks = []
+    for e in df['Entry'].values:
+        chunks.append(e)
+    to_download.append(chunks)
 
 # Use ThreadPool for multithreading
-pool = ThreadPool(20) # Or how many threads you have 
+pool = ThreadPool() # Or how many threads you have 
 
 # Map tasks to threads
-results = pool.map(run, [to_download])
+results = pool.map(run, to_download)
 
 # Close the pool and wait for tasks to complete
 pool.close()
